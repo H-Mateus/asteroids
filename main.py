c76264826d181afd10b65f3af9ef3769f758f48b
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2025-07-15 08:48:52 +0100

    Add a clock to limit frame rate to 60fps

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Create the screen object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Create clock object and delta variable
+    clock = pygame.time.Clock()
+    dt = 0
 
     while 1 > 0:
         for event in pygame.event.get():
@@ -19,6 +24,8 @@ def main():
                 return
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
