b1f8c838c651dc5a012f9031b706fad9ce50982f
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2025-07-15 10:04:31 +0100

    Add asteroid field generation

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 
+import asteroidfield
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -16,7 +19,11 @@ def main():
     # Create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     # Create the screen object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -26,6 +33,8 @@ def main():
 
     # create the player
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    # create the asteroid field
+    asteroid_field = AsteroidField()
 
     while 1 > 0:
         for event in pygame.event.get():
